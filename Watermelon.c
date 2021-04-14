db66dd9b185262760cd887c079e81fa632da4bb1
Tamal <69917079+Ta-Ah@users.noreply.github.com>
2021-04-14 22:42:19 +0600

    Add files via upload

---
+++ Watermelon.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+{    int n; 
+      scanf("%d", &n);
+      if(n <= 2 || n%2 != 0)         
+      {
+          printf("NO\n");
+     }
+     else
+    {
+         printf("YES\n");
+    }
+    return 0;
+}
